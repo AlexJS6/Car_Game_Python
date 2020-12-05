835332fa667b1b4d9ee6939772528d558a49dc8f
AlexJS6 <alex.spiesberger@gmail.com>
2020-12-05 21:33:49 +0100

    starting project and adding the classes

--- main.py
+++ main.py
@@ -19,3 +19,24 @@ GREEN_CAR = pygame.image.load(os.path.join('assets', 'green.png'))
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'background.png')), (WIDTH, HEIGHT))
 
+class Car:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+    
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def get_width():
+        return self.img
+
+    
+def main():
+    run = True
+    FPS = 60
+    lives = 3
+    main_font = pygame.font.SysFont('comicsans', 50)
+    lost_font = pygame.font.SysFont('comicsans', 60)
+    
