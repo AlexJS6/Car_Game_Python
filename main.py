5f59437895546021b88d5ffcf4a46529264ef2fc
AlexJS6 <alex.spiesberger@gmail.com>
2020-12-06 18:55:40 +0100

    Adding music + main_menu() in commentary

--- main.py
+++ main.py
@@ -154,8 +154,20 @@ def main():
                 enemies.remove(enemy)
             
 # maybe launch in waves
-def main_menu():
-    pass
+'''def main_menu():
+    title_font = pygame.font.SysFont("comicsans", 70)
+    run = True
+    while run:
+        WIN.blit(BG, (0,0))
+        title_label = title_font.render("Press the mouse to begin...", 1, (255,255,255))
+        WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, 350))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+    pygame.quit()'''
 
 main()
 
